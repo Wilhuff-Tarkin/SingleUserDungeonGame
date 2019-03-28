68ede8eee16ef761557f326a993d74679af75db3
Wilhuff-Tarkin <giantcaterpillar@gmail.com>
2019-03-28 11:01:44 +0100

    test after system crash

--- src/Map/Map.java
+++ src/Map/Map.java
@@ -3,8 +3,13 @@ package Map;
 public class Map {
 
 
-    MapTile koniec = new MapTile(0, "nie mozesz tam isc", null, null, null, null);
-  //  MapTile start = new MapTile(1, "punkt startowy", korytarz1, koniec, koniec, koniec);
-  //  MapTile korytarz1 = new MapTile(2, "korytarz", koniec, start, koniec, koniec);
+    MapTile start = new MapTile(0, "startowa lokacja");
+    MapTile koniec = new MapTile(4, "to jest koniec drogi");
+    MapTile korytarz1 = new MapTile(1, "ciemny korytarz");
+
+    MapTile [][] glownaMapa = new MapTile[2][2];
+
+    
+
 
 }
--- src/Map/MapTile.java
+++ src/Map/MapTile.java
@@ -5,17 +5,19 @@ public class MapTile {
     int id;
     String description;
 
-    MapTile north;
-    MapTile south;
-    MapTile east;
-    MapTile west;
+//    MapTile north;
+//    MapTile south;
+//    MapTile east;
+//    MapTile west;
 
-    public MapTile(int id, String description, MapTile north, MapTile south, MapTile east, MapTile west) {
+    //, MapTile north, MapTile south, MapTile east, MapTile west
+
+    public MapTile(int id, String description) {
         this.id = id;
         this.description = description;
-        this.north = north;
-        this.south = south;
-        this.east = east;
-        this.west = west;
+//        this.north = north;
+//        this.south = south;
+//        this.east = east;
+//        this.west = west;
     }
 }
